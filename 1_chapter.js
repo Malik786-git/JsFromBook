30574a59cd313a109a0d081a49ea4379337d757c
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-09-22 03:37:59 +0500

    js from book

---
+++ 1_chapter.js
@@ -0,0 +1,64 @@
+// template literals
+
+// var a = 10;
+
+// console.log("this is a")
+// console.log(`this is ${a}`)
+// console.log("this is ${a}")
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// var b=9.8;
+// console.log(typeof(b))
+// console.log(- (10 - 2))
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// console.log(5>22);
+// console.log("ashar ahmed" < "malik muhammad")
+// console.log("ashar" == "ashar")
+// console.log("10" == 10)
+// console.log(10 === "10")
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// There is only one value in JavaScript that is not equal to itself, and that is
+// NaN (“not a number”).
+// console.log(NaN == NaN) 
+// console.log(null == null)
+// console.log(undefined == null)
+// console.log(undefined == undefined)
+
+
+////////////////////////////////////////////////////////////
+// console.log(true || false) //or
+// console.log(true && false) // and
+/////////////////////////////////////////////////////////////
+// var x = null, y;
+// console.log(x, y)
+
+
+////////////////////////////////////////////////////////////
+// Automatic type conversion
+/////////////////////////////////////////////////////////
+// In the Introduction, I mentioned that JavaScript goes out of its way to accept
+// almost any program you give it, even programs that do odd things. This is
+// nicely demonstrated by the following expressions:
+// console.log(null)
+// null
+// console.log(null == 0);
+// → false
+// console.log(8 * null)
+// // → 0
+// console.log("5" - 1)
+// // → 4
+// console.log("5" + 1)
+// // → 51
+// console.log("five" * 2)
+// // → NaN
+// console.log(false == 0)
+// // → true
+// console.log(undefined)
+//undefine
+// console.log(null == undefined);
+// → true
+// console.log(8 * undefine)
+// NaN
+
+
---
+++ 2_chapter.js
@@ -0,0 +1,62 @@
+// what type of name of variable allow 
+// A binding name may include dollar signs ($) or underscores (_) but no other
+// punctuation or special characters.
+// let $name = "ashar ahmed";
+// console.log($name);
+
+// // Camel case
+// var studentNameRoll = "malik jahangir";
+// // Pascal case
+// var StudentNameRoll = "ashar ahmed";
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// if (true) {
+//     console.log("malik");
+// }else{
+//     console.log("ashar");
+// }
+
+// console.log(false ? "malik" : "ashar");
+
+// console.log(true ? 1 : 2);
+// // → 1
+// console.log(false ? 1 : 2);
+// // → 2
+
+
+
+// />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let fact = 1;
+// let i = 1;
+// while (i<=5) {
+//     fact = fact*i;
+//     i++;
+// }
+// console.log(fact);
+
+
+// let fact = 1;
+// let i = 1;
+// do{
+//     fact = fact*i;
+//     i++;
+// }while(i<=5)
+// console.log(fact);
+
+// let fact = 1;
+// for (let i = 1; i <=5; i++) {
+//     fact = fact*i;
+// }
+// console.log(fact);
+
+
+
+
+
+
+
+
+
