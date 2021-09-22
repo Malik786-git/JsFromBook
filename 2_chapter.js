175df03770bb5c6c09d7c0155d3851c5bab85fae
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-09-22 18:38:30 +0500

    chapter 2

--- 2_chapter.js
+++ 2_chapter.js
@@ -28,7 +28,7 @@
 
 
 // />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+// loop
 // let fact = 1;
 // let i = 1;
 // while (i<=5) {
@@ -52,11 +52,77 @@
 // }
 // console.log(fact);
 
+// />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+// for (let current = 20; ; current = current + 1) {
+//     console.log(current);
+   
+//     if (current % 7 == 0) {
+//     console.log(current);
+//     break;
+//     }
+//     }
 
+// var weather = "cloudy"
+// switch (weather) {
+//     case "rainy":
+//     console.log("Remember to bring an umbrella.");
+//     break;
+//     case "sunny":
+//     console.log("Dress lightly.");
+//     break;
+//     case "cloudy":
+//     console.log("Go outside.");
+//     // break;
+//     default:
+//     console.log("Unknown weather type!");
+//     break;
+//     }
+    
 
+// />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// single line comments
+/*
+mulity
+line comments
+*/ 
 
+// />>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// task 
 
+// for (let i = 0; i < 7; i++) {
+   
+//     for (let j = 0; j <= i ; j++) {
+//             console.log("#");
+//     }
+//     console.log("\n");
+// }
+// solution
+// for (let line = "#"; line.length < 8; line += "#")
+//       console.log(line);
 
+// for (let i = 1; i <= 8; i++) {
+//     for (let str = "" ; str.length < 8; str+=" #" ) {
+//         console.log(str)
+//     }
+// }
+// solution
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "*";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
 
 
